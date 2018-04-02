b23e8332838834bec91d8aa0765b4e196c979ec0
Martin C. Frith <mcfrith@gmail.com>
2018-04-02 21:28:26 +0900

    Let TR expansions align further past the repeat

--- tests/tests.sh
+++ tests/tests.sh
@@ -24,5 +24,6 @@ PATH=..:$PATH
     try tandem-genotypes far.bed far.maf
     try tandem-genotypes -u1 -pI -g refFlat.txt simpleRepeat.txt nano.maf
     try tandem-genotypes -u3 -g refFlat.txt simpleRepeat.txt nano.maf
+    try tandem-genotypes slop3.bed slop3.maf
 } 2>&1 |
 diff -u $(basename $0 .sh).out -
